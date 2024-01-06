aa9f91af1b0c672155265f1cd0f6703f2bda1afa
ovchinikov <sergeyvinokov@gmail.com>
2024-01-06 11:18:55 +0300

    unicafe step5 exercise 1.10

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,24 +4,34 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+// statistics line component
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 // statistics component
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = (good - bad) / all;
-  const positive = (good / all) * 100;
+  const positive = (good / all) * 100 + "%";
 
   return (
     <div>
       <h1>statistics</h1>
       {all > 0 ? (
         <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {all}</p>
-          <p>average {average}</p>
-          <p>positive {positive} %</p>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
         </>
       ) : (
         <p>No feedback given</p>
@@ -34,7 +44,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good + neutral + bad;
 
   // Good button click handler
   const handleGoodClick = () => {
@@ -58,7 +67,7 @@ const App = () => {
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
 
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
