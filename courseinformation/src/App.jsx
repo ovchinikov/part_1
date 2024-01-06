6d9d831047c3d5409c8ff2f46a891561cbbc739f
ovchinikov <sergeyvinokov@gmail.com>
2024-01-06 13:14:25 +0300

    part 6 exercise 2.1

--- courseinformation/src/App.jsx
+++ courseinformation/src/App.jsx
@@ -13,9 +13,9 @@ const Part = ({ part }) => {
 const Content = ({ parts }) => {
   return (
     <>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
+      {parts.map((part) => {
+        return <Part key={part.id} part={part} />;
+      })}
     </>
   );
 };
@@ -38,31 +38,47 @@ const Total = ({ parts }) => {
   );
 };
 
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Debugging React Apps",
+        exercises: 10,
+        id: 4,
       },
     ],
   };
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <>
+      <Course course={course} />
+    </>
   );
 };
 
